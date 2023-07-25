60bcbc4526e4f6f7117704535225f6b169cfd784
vinodkn <vinodkn.kn@honeywell.com>
2023-07-25 18:43:37 +0530

    Updated XCFramework

---
+++ MySampleSDK.xcframework/ios-arm64/MySampleSDK.framework/Headers/MySDK.h
@@ -0,0 +1,18 @@
+//
+//  MySDK.h
+//  MySampleSDK
+//
+//  Created by H454944 on 21/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MySDK : NSObject
+
+-(int)multiply:(int)a with:(int)b;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MySampleSDK.xcframework/ios-arm64/MySampleSDK.framework/Headers/MySampleSDK.h
@@ -0,0 +1,18 @@
+//
+//  MySampleSDK.h
+//  MySampleSDK
+//
+//  Created by H454944 on 21/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MySampleSDK.
+FOUNDATION_EXPORT double MySampleSDKVersionNumber;
+
+//! Project version string for MySampleSDK.
+FOUNDATION_EXPORT const unsigned char MySampleSDKVersionString[];
+#import <MySampleSDK/MySDK.h>
+// In this header, you should import all the public headers of your framework using statements like #import <MySampleSDK/PublicHeader.h>
+
+
